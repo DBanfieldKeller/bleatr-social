453bdbed8d143831eba1ae71b7ffd9e5aecff055
Goat <dpajbk@gmail.com>
2023-05-30 16:05:47 -0400

    added get all bleats route

--- server/controllers/bleatController.js
+++ server/controllers/bleatController.js
@@ -21,6 +21,7 @@ const bleatController = {
             .catch(err => res.status(500).json(err))
     },
 
+    // delete bleat
     deleteBleat(req, res) {
         const validatedToken = auth.userFromToken(req.headers.token)
         if (!validatedToken.verified) {
@@ -42,6 +43,20 @@ const bleatController = {
                 res.status(500).json(err)
             })
 
+    },
+
+    // get recent bleats
+    getRecentBleats(req, res) {
+        Bleat.find({})
+            .populate({ path: 'replies', select: '-__v'})
+            .select('__v')
+            .limit(req.query.bleatNumber)
+            .sort({createdAt: -1})
+            .then(dbBleatData => res.json(dbBleatData))
+            .catch(err => {
+                console.log(err)
+                res.status(500).json(err)
+            });
     }
 };
 
--- server/routes/api/bleatRoutes.js
+++ server/routes/api/bleatRoutes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
-const {createBleat, deleteBleat} = require('../../controllers/bleatController');
+const {
+    createBleat,
+    deleteBleat,
+    getRecentBleats
+} = require('../../controllers/bleatController');
 
 // /api/bleats/
 router.route('/').post(createBleat);
 router.route('/:id').delete(deleteBleat)
+router.route('/recent').get(getRecentBleats)
 
 module.exports = router
