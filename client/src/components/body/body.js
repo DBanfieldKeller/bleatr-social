83e7728d7b468237d8c47d08262dd896cf548352
Goat <dpajbk@gmail.com>
2023-06-30 12:20:39 -0400

    placeholder commit

--- client/src/components/body/body.js
+++ client/src/components/body/body.js
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import BleatContainer from '../bleat-container/bleatContainer';
 import HomePage from '../home-page/homePage';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
 
 import './style.css'
 
